fa12407b00219f1979cea1b9744c0dcc09d40726
Vania <vania.almeida28@hotmail.com>
2021-12-11 18:49:49 -0300

    iniciando projeto

---
+++ app/src/main/java/br/com/vaniala/businesscard/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.vaniala.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.vaniala.businesscard.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
